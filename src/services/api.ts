17600b688b1493e6507df8c6f4163d9d0a6c8656
TWDavidDai <weijian.dai@thoughtworks.com>
2024-03-13 16:27:59 +0800

    [UNCAIM-86][David] feat: add base request instance and interceptor and error handler

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import type { AxiosPromise, AxiosRequestConfig } from "axios";
 import axios from "axios";
 
-const prefix = "http://localhost:4000";
+const baseUrl = "http://localhost:4000";
 
 export async function requestWithPrefix<T>(
   url: string,
@@ -15,6 +15,39 @@ export async function requestWithPrefix<T>(
 }
 
 export async function request<T>(url: string, config?: AxiosRequestConfig): Promise<AxiosPromise<T>> {
-  const response: Promise<AxiosPromise<T>> = requestWithPrefix(url, prefix, config);
+  const response: Promise<AxiosPromise<T>> = requestWithPrefix(url, baseUrl, config);
   return await response;
 }
+
+export const instance = axios.create({
+  baseURL: baseUrl, // 设置基本的 API 地址
+  timeout: 5000, // 设置请求超时时间
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = "Bearer " + localStorage.getItem("token");
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401 || error.response.status === 403) {
+        console.log("Unauthorized or forbidden");
+      } else if (error.response.status === 404) {
+        console.log("Not found");
+      } else if (error.response.status >= 500) {
+        console.log("Server error");
+      }
+    }
+    return Promise.reject(error);
+  },
+);
--- src/services/useUserService.ts
+++ src/services/useUserService.ts
@@ -1,8 +1,19 @@
-import { request } from "./api";
+import { instance, request } from "./api";
 import { IUser } from "../types/user";
 
 export const getUserDetail = async () => {
   return await request<IUser>(`/user`, {
     method: "GET",
   });
+};
+
+export const getUserName = () => {
+  instance
+    .get("/user")
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return Promise.reject(error);
+    });
 };
