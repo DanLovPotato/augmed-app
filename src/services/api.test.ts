1056452fde38a56cb25e14e651caf0658f2a1cc1
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-03-21 11:56:01 +0800

    [UNCAIM-88][Zhenjia Zhou] test: add test for interceptor

--- src/services/api.test.ts
+++ src/services/api.test.ts
@@ -1,10 +1,27 @@
 import { instance } from "./api";
 import MockAdapter from "axios-mock-adapter";
 
+const localStorageMock = (function () {
+  let store: { [key: string]: string };
+  return {
+    getItem: function (key: string) {
+      return store[key];
+    },
+    setItem: function (key: string, value: string) {
+      store[key] = value.toString();
+    },
+    clear: function () {
+      store = {};
+    },
+  };
+})();
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
+
 const mock = new MockAdapter(instance);
 
 describe("API Tests", () => {
-  afterEach(() => {
+  beforeEach(() => {
+    localStorage.clear();
     mock.reset();
   });
 
@@ -18,6 +35,26 @@ describe("API Tests", () => {
 
     expect(response.status).toBe(200);
     expect(response.data).toEqual(responseData);
+  });
+
+  test("Authorization header is correctly set in request", async () => {
+    const token = "123456";
+    localStorage.setItem("token", token);
+    const url = "/protected";
+    mock.onGet(url).reply((config) => {
+      expect(config.headers?.Authorization).toEqual(`Bearer ${token}`);
+      return [200];
+    });
+
+    await instance.get(url);
+  });
+
+  test("Token is stored on receiving new token", async () => {
+    const newToken = "newToken";
+    mock.onGet("/api/users").reply(200, {}, { Authorization: `Bearer ${newToken}` });
+
+    await instance.get("/api/users");
+    expect(localStorage.getItem("token")).toEqual(newToken);
   });
 
   test("Error handling interceptor", async () => {
