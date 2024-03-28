89518161f2ce388612186c1c3d7687a421fdbe72
TWDavidDai <weijian.dai@thoughtworks.com>
2024-03-28 16:36:36 +0800

    [TECH][David] fix: fix eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    "cypress/globals": true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:cypress/recommended",
+  ],
   overrides: [
     {
       env: {
