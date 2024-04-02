b39517b7507027c909a5b1c10bd6aa3907d3039a
TWDavidDai <weijian.dai@thoughtworks.com>
2024-04-02 16:35:23 +0800

    [UNCAIM-99][David] feat: implemented CaseCard & homepage

---
+++ src/components/CaseCard/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./index.module.scss";
+import { ICase } from "../../types/case";
+
+interface CaseCardProps {
+  patientCase: ICase;
+  onClick?: () => void;
+}
+
+const CaseCard = ({ patientCase, onClick }: CaseCardProps) => {
+  return (
+    <div className={styles.caseCardContainer} onClick={onClick}>
+      <div className={styles.caseIdContainer}>
+        <span>{patientCase.id}</span>
+      </div>
+      <div className={styles.chiefComplaintContainer}>
+        <span>{patientCase.patient_chief_complaint}</span>
+      </div>
+      <div className={styles.patientInfoContainer}>
+        <span>{patientCase.age + ", " + patientCase.sex}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CaseCard;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,62 @@
 import React from "react";
 import styles from "./index.module.scss";
-import { useRequest } from "ahooks";
-import { getHealthCheck } from "../../services/healthCheckService";
+import { useMount, useRequest } from "ahooks";
+import CaseCard from "../../components/CaseCard";
+import { getCaseList } from "../../services/caseService";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+import { ICase } from "../../types/case";
+import AssignmentLateIcon from "@mui/icons-material/AssignmentLate";
+
+const useGetCaseList = () => {
+  const { loading, runAsync, data } = useRequest(getCaseList, {
+    manual: true,
+  });
+  return { loading, getCaseList: runAsync, cases: data?.data };
+};
 
 const ExampleHomePage = () => {
-  const { loading, data } = useRequest(getHealthCheck);
+  const { getCaseList, loading } = useGetCaseList();
+  const [dataSource, setDataSource] = React.useState<ICase[] | null>(null);
+
+  useMount(() => {
+    getList();
+  });
+
+  const getList = () => {
+    getCaseList()
+      .then((response) => {
+        const newData = response?.data || [];
+        setDataSource(newData);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
 
   return (
-    <>
-      <div className={styles.app}>Home page: </div>
-      <div>{loading ? "loading" : JSON.stringify(data)}</div>
-    </>
+    <div className={styles.app}>
+      <div className={styles.titleContainer}>
+        <span className={styles.title}>Pending Cases</span>
+      </div>
+      {loading && dataSource === null ? (
+        <Box className={`${styles.loading} ${styles.centered}`}>
+          <CircularProgress />
+        </Box>
+      ) : !loading && dataSource?.length === 0 ? (
+        <div className={styles.empty}>
+          <AssignmentLateIcon className={styles.icon} />
+          <span>
+            There is no available tasks for you now. <a href="mailto:dhep.lab@gmail.com">dhep.lab@gmail.com</a> to get
+            new tasks. Or try to refresh the page.
+          </span>
+        </div>
+      ) : (
+        <div className={styles.pendingCasesContainer}>
+          {dataSource?.map((item) => <CaseCard key={item.id} patientCase={item} />)}
+        </div>
+      )}
+    </div>
   );
 };
 
