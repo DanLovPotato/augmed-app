da0f91612c313225fadce3e2713658addc0724be
TWDavidDai <weijian.dai@thoughtworks.com>
2024-04-09 14:15:30 +0800

    [TECH][David] refactor: remove unused component and page

--- src/components/ExampleComponent/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import ExampleComponent from "./index";
-
-test("renders text", () => {
-  render(<ExampleComponent />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
