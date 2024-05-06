d02f93992bb6ee36f0877fe44ec6e9408499b42e
zhenjiazhou <zhenjia.zhou@thoughtworks.com>
2024-05-06 16:40:15 +0800

    [UNCAIM-39][Zhenjia Zhou] feat: add navigation do case detail

--- src/components/CaseCard/index.tsx
+++ src/components/CaseCard/index.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import { ICase } from "../../types/case";
+import path from "../../routes/path";
+import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import { getCaseDetail } from "../../services/caseService";
+import { useNavigate } from "react-router-dom";
 
 interface CaseCardProps {
   className?: string;
@@ -9,8 +13,24 @@ interface CaseCardProps {
 }
 
 const CaseCard = ({ className, patientCase, onClick }: CaseCardProps) => {
+  const nav = useNavigate();
+  const [, setSlot] = useState<React.ReactNode>(null);
+
+  const handleOnClick = () =>
+    getCaseDetail(patientCase.case_id)
+      .then(() => {
+        nav(path.case);
+      })
+      .catch((error) => {
+        setSlot(
+          <div className={styles.errorContainer}>
+            <ErrorOutlineIcon />
+            <span className={styles.errorMessage}>{error.message}</span>
+          </div>,
+        );
+      });
   return (
-    <div className={`${styles.caseCardContainer} ${className}`} onClick={onClick}>
+    <div className={`${styles.caseCardContainer} ${className} `} onClick={handleOnClick}>
       <div className={styles.caseIdContainer}>
         <span>Case: {patientCase.case_id}</span>
       </div>
